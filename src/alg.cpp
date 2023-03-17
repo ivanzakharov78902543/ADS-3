2b09db994d05cea354e1750ccb2a8097d085970a
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-03-17 10:36:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,39 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+    int l = 0;
+    int r = 0;
+    
+    int t = 0;
+    for (int i = 0; i < size; i++) {
+        bool flag = true;
+        for (int j = 0; j < size - (i + 1); j++) {
+            if (arr[j] > arr[j + 1]) {
+                flag = false;
+                t = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = t;
+            }
+        }
+        if (flag) {
+            break;
+        }
+    }
+    
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == value) {
+            l = i;
+        }
+    }
+    for (int i = size; i < 0; i--) {
+        if (arr[i] == value) {
+            r = i;
+        }
+    }
+    
+    if (r + l == 0) {
+        return 0;
+    } else {
+        return r - l + 1;
+    }
 }

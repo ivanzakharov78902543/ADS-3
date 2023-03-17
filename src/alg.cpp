fd10c3ac3e3c318a3dc9d39423b1e8d3dabc8f44
ivanzakharov78902543 <125127109+ivanzakharov78902543@users.noreply.github.com>
2023-03-17 11:33:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,9 +1,12 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
+    int l = 0;
+    int r = size - 1;
+    int mid = 0;
     int k = 0;
-    int w = 0;
     int t = 0;
+    bool f = true;
     for (int i = 0; i < size; i++) {
         bool flag = true;
         for (int j = 0; j < size - (i + 1); j++) {
@@ -18,21 +21,31 @@ int cbinsearch(int *arr, int size, int value) {
             break;
         }
     }
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == value) {
-            w = i;
+    while ((l <= r) && (f)) {
+        mid = (l + r) / 2;
+        if (arr[mid] == value) {
+            f = false;
+            for (int i = mid; i <= r; i++) {
+                if (arr[i] == value) {
+                    k += 1;
+                }
+            }
+            for (int i = mid; i >= l; i--) {
+                if (arr[i] == value) {
+                    k += 1;
+                }
+            }
+        }
+        if (arr[mid] > value) {
+            r = mid - 1;
+        }
+        else {
+            l = mid + 1;
         }
     }
-    for (int i = w; i < size; i++) {
-        if (arr[i] == value) {
-            k += 1;
-        } else {
-            break;
-        }
+    if (f) {
+        return k;
     }
-    if (k == 0) {
+    else {
         return 0;
-    } else {
-        return k; 
     }
-}
